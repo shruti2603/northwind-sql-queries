0fd4358833c4eef90d4224e6fc938195af422374
shruti2603 <91823057+shruti2603@users.noreply.github.com>
2023-06-11 14:12:13 +0530

    Add files via upload

---
+++ query2.sql
@@ -0,0 +1,4 @@
+-- 1.	Create a report that shows the CategoryName and Description from the categories table sorted by CategoryName.
+SELECT CategoryName, Description
+FROM dbo.Categories
+ORDER BY CategoryName
